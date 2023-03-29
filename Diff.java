0fa063ca6d3c4071e4efca170e11eba776abe8e4
ammshreya <shreyunayak24@gmail.com>
2023-03-29 19:24:44 +0530

    added msg to sum.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,6 @@ public class Diff {
     public static void name(String args[]) {
       int a=15;
         int b=5;
-        System.out.println(a-b);
+        System.out.println("diff"+(a-b));
     }
 }
